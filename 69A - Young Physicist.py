c4937e864d4e9eae4c00857e24d2f452781c4773
Tamas Raj <tamas@Tamass-MacBook-Air.local>
2020-06-01 21:35:54 +0530

    Remove 69A

--- 69A - Young Physicist.py
+++
@@ -1,9 +0,0 @@
-n = input()
-I , J , K = 0 , 0 , 0
-for _ in range(int(n)):
-    i , j , k = map(int , input().split())
-    I += i
-    J += j
-    K += k
- 
-print("YES" if I == 0 and J == 0 and K == 0 else "NO")
