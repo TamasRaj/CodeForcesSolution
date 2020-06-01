1116100ea758fbcfca23d21b79165e1875954549
Tamas Raj <tamas@Tamass-MacBook-Air.local>
2020-06-01 21:14:39 +0530

    69A

---
+++ 69A - Young Physicist.py
@@ -0,0 +1,9 @@
+n = input()
+I , J , K = 0 , 0 , 0
+for _ in range(int(n)):
+    i , j , k = map(int , input().split())
+    I += i
+    J += j
+    K += k
+ 
+print("YES" if I == 0 and J == 0 and K == 0 else "NO")
